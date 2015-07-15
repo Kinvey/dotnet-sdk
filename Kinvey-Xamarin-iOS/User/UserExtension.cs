006566f34060aff15ab120e6b68835b0b39c0a82
Edward Fleming <ed@kinvey.com>
2015-07-15 15:53:03 -0400

    MLIBZ-466 cleaning up the iOS MIC implementation

---
+++ Kinvey-Xamarin-iOS/User/UserExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KinveyXamarin;
+using Foundation;
+
+namespace KinveyXamariniOS
+{
+	public static class UserExtension
+	{
+		public static bool OnOAuthCallbackRecieved(this AsyncUser user, NSUrl url){
+			Console.WriteLine (url.Query);
+			Console.WriteLine (url.Query.Substring(url.Query.IndexOf ("code=") + 5) );
+			string accesstoken = url.Query.Substring(url.Query.IndexOf ("code=") + 5) ;
+			user.GetMICAccessToken(accesstoken);
+			return true;
+		}
+	}
+}
+
