a74712fc91f63ca125a64f844c7c5c11f526673f
Edward Fleming <ed@kinvey.com>
2015-07-29 15:53:12 -0400

    1.5.2 release

--- Kinvey-Xamarin/Core/KinveyHeaders.cs
+++ Kinvey-Xamarin/Core/KinveyHeaders.cs
@@ -28,7 +28,7 @@ namespace KinveyXamarin
 		/// <summary>
 		/// The version of the library.
 		/// </summary>
-		public static string VERSION = "1.5.1";
+		public static string VERSION = "1.5.2";
 
 		/// <summary>
 		/// The kinvey API version key.
