4dbbbe7b5668496e79e5dd8e3e29eef8d48549b8
Victor Barros <victor@kinvey.com>
2015-11-16 17:18:34 -0800

    Xamarin 1.5.6 Release

--- Kinvey-Xamarin/Core/KinveyHeaders.cs
+++ Kinvey-Xamarin/Core/KinveyHeaders.cs
@@ -28,7 +28,7 @@ namespace KinveyXamarin
 		/// <summary>
 		/// The version of the library.
 		/// </summary>
-		public static string VERSION = "1.5.5";
+		public static string VERSION = "1.5.6";
 
 		/// <summary>
 		/// The kinvey API version key.
