9dda864e9ae36d83338aeed736cd7958f214c448
Vinay Gahlawat <vinay@kinvey.com>
2016-03-10 17:27:56 -0500

    Release 1.6.8

--- Kinvey-Xamarin/Core/KinveyHeaders.cs
+++ Kinvey-Xamarin/Core/KinveyHeaders.cs
@@ -28,7 +28,7 @@ namespace KinveyXamarin
 		/// <summary>
 		/// The version of the library.
 		/// </summary>
-		public static string VERSION = "1.6.7";
+		public static string VERSION = "1.6.8";
 
 		/// <summary>
 		/// The kinvey API version key.
