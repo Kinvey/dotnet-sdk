9f1e8131f64c01502cbdd690e3ec3d27dece69c6
Vinay Gahlawat <vinay@kinvey.com>
2016-04-22 10:20:33 -0400

    Added user unit tests around social login.

--- UnitTests/TestUser.cs
+++ UnitTests/TestUser.cs
@@ -47,8 +47,23 @@ namespace UnitTestFramework
 			// Assert
 		}
 
-		// LOGIN/LOGOUT TESTS
-		//
+		#region Login/Logout Tests
+
+		[Test]
+		public async Task TestLoginAsync()
+		{
+			// Arrange
+
+			// Act
+			await kinveyClient.CurrentUser.LoginAsync();
+
+			// Assert
+			Assert.NotNull(kinveyClient.CurrentUser);
+			Assert.True(kinveyClient.CurrentUser.isUserLoggedIn());
+
+			// Teardown
+			kinveyClient.CurrentUser.Logout();
+		}
 
 		[Test]
 		public async Task TestLoginAsyncBad()
@@ -65,22 +80,6 @@ namespace UnitTestFramework
 			Assert.Catch(async delegate() {
 				await fakeClient.CurrentUser.LoginAsync(user, pass);
 			});
-		}
-
-		[Test]
-		public async Task TestLoginAsync()
-		{
-			// Arrange
-
-			// Act
-			await kinveyClient.CurrentUser.LoginAsync();
-
-			// Assert
-			Assert.NotNull(kinveyClient.CurrentUser);
-			Assert.True(kinveyClient.CurrentUser.isUserLoggedIn());
-
-			// Teardown
-			kinveyClient.CurrentUser.Logout();
 		}
 
 		[Test]
@@ -101,56 +100,233 @@ namespace UnitTestFramework
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
+		public async Task TestLoginUserPassAsyncBad()
+		{
+			// Arrange
+
+			// Act
+
+			// Assert
+		}
+
+		[Test]
+		[Ignore("Placeholder - Need Access Token To Run Test")]
 		public async Task TestLoginFacebookAsync()
 		{
+			// Arrange
+			string facebookAccessToken = "";
+
+			// Act
+			User fbUser = await kinveyClient.CurrentUser.LoginFacebookAsync(facebookAccessToken);
+
+			// Assert
+			Assert.IsNotNull(fbUser);
+			Assert.IsNotNull(fbUser.Attributes["_socialIdentity"]);
+			JToken socID = fbUser.Attributes["_socialIdentity"];
+			Assert.IsNotNull(socID["facebook"]);
+			Assert.True(socID["facebook"].HasValues);
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestLoginFacebookAsyncBad()
 		{
+			// Arrange
+			string facebookAccessTokenBad = "blahblahblah";
+
+			// Act
+			// Assert
+			Assert.Catch(async delegate() {
+				await kinveyClient.CurrentUser.LoginFacebookAsync(facebookAccessTokenBad);
+			});
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
+		[Ignore("Placeholder - Need Access Token To Run Test")]
 		public async Task TestLoginGoogleAsync()
 		{
+			// Arrange
+			string googleAccessToken = "";
+
+			// Act
+			User googleUser = await kinveyClient.CurrentUser.LoginGoogleAsync(googleAccessToken);
+
+			// Assert
+			Assert.IsNotNull(googleUser);
+			Assert.IsNotNull(googleUser.Attributes["_socialIdentity"]);
+			JToken socID = googleUser.Attributes["_socialIdentity"];
+			Assert.IsNotNull(socID["google"]);
+			Assert.True(socID["google"].HasValues);
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestLoginGoogleAsyncBad()
 		{
+			// Arrange
+			string googleAccessTokenBad = "blahblahblah";
+
+			// Act
+			// Assert
+			Assert.Catch(async delegate() {
+				await kinveyClient.CurrentUser.LoginGoogleAsync(googleAccessTokenBad);
+			});
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
+		[Ignore("Placeholder - Need Access Token To Run Test")]
 		public async Task TestLoginTwitterAsync()
 		{
+			// Arrange
+			string accessTokenKey = "";
+			string accessTokenSecret = "";
+			string consumerKey = "";
+			string consumerKeySecret = "";
+
+			// Act
+			User twitterUser = await kinveyClient.CurrentUser.LoginTwitterAsync(accessTokenKey, accessTokenSecret, consumerKey, consumerKeySecret);
+
+			// Assert
+			Assert.IsNotNull(twitterUser);
+			Assert.IsNotNull(twitterUser.Attributes["_socialIdentity"]);
+			JToken socID = twitterUser.Attributes["_socialIdentity"];
+			Assert.IsNotNull(socID["twitter"]);
+			Assert.True(socID["twitter"].HasValues);
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestLoginTwitterAsyncBad()
 		{
+			// Arrange
+			string accessTokenKey = "twitterAccessTokenBad";
+			string accessTokenSecret = "twitterAccessTokenSecretBad";
+			string consumerKey = "twitterConsumerKeyBad";
+			string consumerKeySecret = "twitterConsumerKeySecretBad";
+
+			// Act
+			// Assert
+			Assert.Catch(async delegate() {
+				await kinveyClient.CurrentUser.LoginTwitterAsync(accessTokenKey, accessTokenSecret, consumerKey, consumerKeySecret);
+			});
 		}
 
 		[Test]
-		[Ignore("Placeholder - No unit test yet")]
+		[Ignore("Placeholder - Need Access Token To Run Test")]
+		public async Task TestLoginLinkedInAsync()
+		{
+			// Arrange
+			string accessTokenKey = "";
+			string accessTokenSecret = "";
+			string consumerKey = "";
+			string consumerKeySecret = "";
+
+			// Act
+			User linkedinUser = await kinveyClient.CurrentUser.LoginLinkedinAsync(accessTokenKey, accessTokenSecret, consumerKey, consumerKeySecret);
+
+			// Assert
+			Assert.IsNotNull(linkedinUser);
+			Assert.IsNotNull(linkedinUser.Attributes["_socialIdentity"]);
+			JToken socID = linkedinUser.Attributes["_socialIdentity"];
+			Assert.IsNotNull(socID["linkedin"]);
+			Assert.True(socID["linkedin"].HasValues);
+		}
+
+		[Test]
+		public async Task TestLoginLinkedInAsyncBad()
+		{
+			// Arrange
+			string accessTokenKey = "twitterAccessTokenBad";
+			string accessTokenSecret = "twitterAccessTokenSecretBad";
+			string consumerKey = "twitterConsumerKeyBad";
+			string consumerKeySecret = "twitterConsumerKeySecretBad";
+
+			// Act
+			// Assert
+			Assert.Catch(async delegate() {
+				await kinveyClient.CurrentUser.LoginLinkedinAsync(accessTokenKey, accessTokenSecret, consumerKey, consumerKeySecret);
+			});
+		}
+
+		[Test]
+		[Ignore("Placeholder - Need Access Token To Run Test")]
 		public async Task TestLoginSalesforceAsync()
 		{
+			// Arrange
+			string access = "";
+			string reauth = "";
+			string clientID = "";
+			string ID = "";
+
+			// Act
+			User salesforceUser = await kinveyClient.CurrentUser.LoginSalesforceAsync(access, reauth, clientID, ID);
+
+			// Assert
+			Assert.IsNotNull(salesforceUser);
+			Assert.IsNotNull(salesforceUser.Attributes["_socialIdentity"]);
+			JToken socID = salesforceUser.Attributes["_socialIdentity"];
+			Assert.IsNotNull(socID["salesforce"]);
+			Assert.True(socID["salesforce"].HasValues);
 		}
+
+		[Test]
+		public async Task TestLoginSalesforceAsyncBad()
+		{
+			// Arrange
+			string access = "";
+			string reauth = "";
+			string clientID = "";
+			string ID = "";
+
+			// Act
+			// Assert
+			Assert.Catch(async delegate() {
+				await kinveyClient.CurrentUser.LoginSalesforceAsync(access, reauth, clientID, ID);
+			});
+		}
+
+		// MIC LOGIN TESTS
+		//
+		[Test]
+		public async Task TestMIC_LoginWithAuthorizationCodeLoginPage()
+		{
+			// Arrange
+			string redirectURI = "http://test.redirect";
+
+			// Act
+			string renderURL = await kinveyClient.CurrentUser.LoginWithAuthorizationCodeLoginPage(redirectURI);
+
+			// Assert
+			Assert.IsNotNullOrEmpty(renderURL);
+			Assert.True(renderURL.StartsWith(kinveyClient.MICHostName + "oauth/auth?client_id"));
+		}
+
+		[Test]
+		[Ignore("Placeholder - Need configured backend to run test")]
+		public async Task TestMIC_LoginWithAuthorizationCodeAPI()
+		{
+			// Arrange
+			string username = "testuser";
+			string password = "testpass";
+			string redirectURI = "kinveyAuthDemo://";
+			string saml_app_key = "kid_ZkPDb_34T";
+			string saml_app_secret = "c3752d5079f34353ab89d07229efaf63";
+			Client localClient = new Client.Builder(saml_app_key, saml_app_secret).build();
+			localClient.MICApiVersion = "v2";
+
+			// Act
+			await localClient.CurrentUser.LoginWithAuthorizationCodeAPI(username, password, redirectURI);
+
+			// Assert
+			Assert.True(localClient.CurrentUser.isUserLoggedIn());
+
+			// Teardown
+			localClient.CurrentUser.Logout();
+		}
+
+		#endregion
+
+		#region CRUD Tests
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
-		public async Task TestLoginSalesforceAsyncBad()
-		{
-		}
-
-		// CREATE TESTS
-		//
-
-		[Test]
 		public async Task TestCreateUserAsync()
 		{
 			// Setup
@@ -185,8 +361,6 @@ namespace UnitTestFramework
 			// Assert
 		}
 
-		// READ TESTS
-		//
 		[Test]
 		public async Task TestFindUserAsync()
 		{
@@ -210,6 +384,11 @@ namespace UnitTestFramework
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestFindUserAsyncBad()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
 
 		[Test]
@@ -237,68 +416,79 @@ namespace UnitTestFramework
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestLookupUsersAsyncBad()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
 
-		// UPDATE TESTS
-		//
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestUpdateUserAsync()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestUpdateUserAsyncBad()
 		{
-		}
+			// Arrange
 
-		// DELETE TESTS
-		//
+			// Act
+
+			// Assert
+		}
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestDeleteUserSoftAsync()
 		{
-//			// Arrange
-//			string userID = "4567";
-//
-//			// Act
-//			User.DeleteRequest deleteRequest = await kinveyClient.User().DeleteAsync(userID, false);
-//
-//			// Assert
-//			Assert.True(deleteRequest.RequestMethod == "DELETE");
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
 		public async Task TestDeleteUserSoftAsyncBad()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
 
 		[Test]
+		[Ignore("Placeholder - No unit test yet")]
 		public async void TestDeleteUserHardAsync()
 		{
-			// Setup
-//			kinveyClient.CurrentUser.Logout();
-			await kinveyClient.CurrentUser.LoginAsync(newuser, newpass);
-
 			// Arrange
-			string userID = "57164d59ef5f18c36bb3faba";
 
 			// Act
-			KinveyDeleteResponse kdr = await kinveyClient.CurrentUser.DeleteAsync(userID, true);
 
 			// Assert
-			Assert.AreEqual(1, kdr.count);
-
-			// Teardown
 		}
 
 		[Test]
 		[Ignore("Placeholder - No unit test yet")]
 		public async void TestDeleteUserHardAsyncBad()
 		{
+			// Arrange
+
+			// Act
+
+			// Assert
 		}
+
+		#endregion
 	}
 }
