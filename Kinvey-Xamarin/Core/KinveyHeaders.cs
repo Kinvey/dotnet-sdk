acac006746769173b68d83cac72d13dd25a2d178
Vinay Gahlawat <vinay@kinvey.com>
2016-08-25 11:38:54 -0400

    Xamarin 3.0.0 MVP-07 Release

--- Kinvey-Xamarin/Core/KinveyHeaders.cs
+++ Kinvey-Xamarin/Core/KinveyHeaders.cs
@@ -28,7 +28,7 @@ namespace KinveyXamarin
 		/// <summary>
 		/// The version of the library.
 		/// </summary>
-		public static string VERSION = "3.0.0-mvp06";
+		public static string VERSION = "3.0.0-mvp07";
 
 		/// <summary>
 		/// The kinvey API version key.
