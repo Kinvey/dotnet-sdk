c59b58adede281afafbbcd36f9a6b8b47571ef5f
Vinay Gahlawat <vinay@kinvey.com>
2016-09-06 13:18:40 -0400

    MLIBZ-1201 Refator: code cleanup and separation of custom endpoint request to separate file.

--- Kinvey-Xamarin/CustomEndpoint/CustomEndpoint.cs
+++ Kinvey-Xamarin/CustomEndpoint/CustomEndpoint.cs
@@ -11,19 +11,20 @@
 // Unauthorized reproduction, transmission or distribution of this file and its
 // contents is a violation of applicable laws.
 
-using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace KinveyXamarin
 {
+	/// <summary>
+	/// Custom endpoint.
+	/// </summary>
 	public class CustomEndpoint<I, O>
 	{
 
 		private AbstractClient client;
 
-		//private string clientAppVersion = null;
+//		private string clientAppVersion = null;
 
 		private JObject customRequestProperties = new JObject();
 
@@ -39,91 +40,96 @@ namespace KinveyXamarin
 //			return this.clientAppVersion;
 //		}
 
-		public void SetCustomRequestProperties(JObject customheaders){
-			this.customRequestProperties = customheaders;
+		/// <summary>
+		/// Sets the custom request properties.
+		/// </summary>
+		/// <param name="customheaders">Customheaders.</param>
+		public void SetCustomRequestProperties(JObject customheaders)
+		{
+			customRequestProperties = customheaders;
 		}
 
-		public void SetCustomRequestProperty(string key, JObject value){
-			if (this.customRequestProperties == null){
-				this.customRequestProperties = new JObject();
+		/// <summary>
+		/// Sets the custom request property.
+		/// </summary>
+		/// <param name="key">Key.</param>
+		/// <param name="value">Value.</param>
+		public void SetCustomRequestProperty(string key, JObject value)
+		{
+			if (customRequestProperties == null)
+			{
+				customRequestProperties = new JObject();
 			}
-			this.customRequestProperties.Add (key, value);
+
+			customRequestProperties.Add (key, value);
 		}
 
-		public void ClearCustomRequestProperties(){
-			this.customRequestProperties = new JObject();
+		/// <summary>
+		/// Clears the custom request properties.
+		/// </summary>
+		public void ClearCustomRequestProperties()
+		{
+			customRequestProperties = new JObject();
 		}
 
-		public JObject GetCustomRequestProperties(){
-			return this.customRequestProperties;
+		/// <summary>
+		/// Gets the custom request properties.
+		/// </summary>
+		/// <returns>The custom request properties.</returns>
+		public JObject GetCustomRequestProperties()
+		{
+			return customRequestProperties;
 		}
 
-		public CustomEndpoint (AbstractClient client)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:KinveyXamarin.CustomEndpoint`2"/> class.
+		/// </summary>
+		/// <param name="client">Client.</param>
+		public CustomEndpoint(AbstractClient client)
 		{
 			this.client = client;
-			this.customRequestProperties = client.GetCustomRequestProperties ();
+			customRequestProperties = client.GetCustomRequestProperties();
 			//this.clientAppVersion = client.GetClientAppVersion ();
 		}
 
-		public CustomCommand executeCustomEndpointBlocking(string endpoint, I input){
+		/// <summary>
+		/// Executes the custom endpoint blocking.
+		/// </summary>
+		/// <returns>The custom endpoint blocking.</returns>
+		/// <param name="endpoint">Endpoint.</param>
+		/// <param name="input">Input.</param>
+		public CustomCommand<I, O> executeCustomEndpointBlocking(string endpoint, I input)
+		{
 			var urlParameters = new Dictionary<string, string>();
 			urlParameters.Add("appKey", ((KinveyClientRequestInitializer)client.RequestInitializer).AppKey);
 			urlParameters.Add("endpoint", endpoint);
 
-			CustomCommand custom = new CustomCommand (client, endpoint, input, urlParameters);
+			CustomCommand<I, O> custom = new CustomCommand<I, O>(client, endpoint, input, urlParameters);
 
 			client.InitializeRequest(custom);
 			//custom.clientAppVersion = this.GetClientAppVersion ();
-			custom.customRequestHeaders = this.GetCustomRequestProperties ();
+			custom.customRequestHeaders = GetCustomRequestProperties();
 			return custom;
 		}
 
-		public CustomCommandArray executeCustomEndpointArrayBlocking(string endpoint, I input){
+		/// <summary>
+		/// Executes the custom endpoint array blocking.
+		/// </summary>
+		/// <returns>The custom endpoint array blocking.</returns>
+		/// <param name="endpoint">Endpoint.</param>
+		/// <param name="input">Input.</param>
+		public CustomCommandArray<I,O> executeCustomEndpointArrayBlocking(string endpoint, I input)
+		{
 			var urlParameters = new Dictionary<string, string>();
 			urlParameters.Add("appKey", ((KinveyClientRequestInitializer)client.RequestInitializer).AppKey);
 			urlParameters.Add("endpoint", endpoint);
 
-			CustomCommandArray custom = new CustomCommandArray (client, endpoint, input, urlParameters);
+			CustomCommandArray<I, O> custom = new CustomCommandArray<I, O>(client, endpoint, input, urlParameters);
 
 			client.InitializeRequest(custom);
 			//custom.clientAppVersion = this.GetClientAppVersion ();
-			custom.customRequestHeaders = this.GetCustomRequestProperties ();
+			custom.customRequestHeaders = GetCustomRequestProperties();
 			return custom;
 		}
-
-
-
-		/// <summary>
-		/// Executes a custom endpoint expecting a single result
-		/// </summary>
-		public class CustomCommand : AbstractKinveyClientRequest<O> {
-			private const string REST_PATH = "rpc/{appKey}/custom/{endpoint}";
-
-			[JsonProperty]
-			public string endpoint;
-
-			public CustomCommand(AbstractClient client, string endpoint, I input, Dictionary<string, string> urlProperties) :
-			base(client, "POST", REST_PATH, input, urlProperties){
-				this.endpoint = endpoint;
-			}
-		}
-
-
-		/// <summary>
-		/// Executes a custom endpoint expecting an array of results
-		/// </summary>
-		public class CustomCommandArray : AbstractKinveyClientRequest<O[]> {
-			private const string REST_PATH = "rpc/{appKey}/custom/{endpoint}";
-
-			[JsonProperty]
-			public string endpoint;
-
-			public CustomCommandArray(AbstractClient client, string endpoint, I input, Dictionary<string, string> urlProperties) :
-			base(client, "POST", REST_PATH, input, urlProperties){
-				this.endpoint = endpoint;
-			}
-		}
-
 	}
 }
-
