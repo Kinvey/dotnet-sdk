01500e2ca22e31ed80b10000946999d933864ae4
tejasranade <tejas.ranade@gmail.com>
2017-01-05 18:36:16 -0500

    Added integration with Travis
    update to include SharpCover code coverage tool
    
    yml changes
    
    moving scripts
    :
    
    ignore code coverage failures

---
+++ TestFramework/Tools/Gaillard.SharpCover.Tests/ProgramTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Gaillard.SharpCover.Tests
+{
+    [TestFixture]
+    public sealed class ProgramTests
+    {
+        private string testTargetExePath;
+        private bool onDotNet;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            onDotNet = Type.GetType("Mono.Runtime") == null;
+
+            string buildCommand;
+            if (onDotNet) {
+                buildCommand = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe";
+                testTargetExePath = @"bin\Debug\TestTarget.exe";
+            } else {
+                buildCommand = "xbuild";
+                testTargetExePath = "bin/Debug/TestTarget.exe";
+            }
+
+            var process = Process.Start(buildCommand, "TestTarget.csproj");
+            process.WaitForExit();
+            Assert.AreEqual(0, process.ExitCode);
+        }
+
+        [Test]
+        public void NoBody()
+        {
+            var config =
+                @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""typeInclude"": "".*Tests.*Event.*""}";
+
+            File.WriteAllText("testConfig.json", config);
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", "testConfig.json" }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        [Test]
+        public void Covered()
+        {
+            var config =
+                @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""typeInclude"": "".*TestTarget"", ""methodInclude"": "".*Covered.*""}";
+
+            File.WriteAllText("testConfig.json", config);
+
+            //write some extraneous hit files to make sure they dont affect run
+            File.WriteAllText(Program.HITS_FILENAME_PREFIX, "doesnt matter");
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", "testConfig.json" }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        [Test]
+        public void UncoveredIf()
+        {
+            var config = @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""methodInclude"": "".*UncoveredIf.*""}";
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(1, Program.Main(new []{ "check" }));
+
+            var missCount = File.ReadLines(Program.RESULTS_FILENAME).Where(l => l.StartsWith(Program.MISS_PREFIX)).Count();
+            var knownCount = File.ReadLines(Program.RESULTS_FILENAME).Count();
+
+            Assert.IsTrue(knownCount > 0);
+            Assert.IsTrue(missCount > 0);
+            Assert.IsTrue(knownCount > missCount);
+        }
+
+        [Test]
+        public void UncoveredLeave()
+        {
+            var config = @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""methodInclude"": "".*UncoveredLeave.*""}";
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(1, Program.Main(new []{ "check" }));
+
+            var missCount = File.ReadLines(Program.RESULTS_FILENAME).Where(l => l.StartsWith(Program.MISS_PREFIX)).Count();
+            var knownCount = File.ReadLines(Program.RESULTS_FILENAME).Count();
+
+            Assert.IsTrue(knownCount > 0);
+            Assert.IsTrue(missCount > 0);
+            Assert.IsTrue(knownCount > missCount);
+        }
+
+        [Test]
+        public void Nested()
+        {
+            var config = @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""typeInclude"": "".*Nested""}";
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        [Test]
+        public void LineExcludes()
+        {
+            var config =
+@"{
+    ""assemblies"": [""bin/Debug/TestTarget.exe""],
+    ""typeInclude"": "".*TestTarget"",
+    ""methodInclude"": "".*LineExcludes.*"",
+    ""methodBodyExcludes"": [
+        {
+            ""method"": ""System.Void Gaillard.SharpCover.Tests.TestTarget::LineExcludes()"",
+            ""lines"": [""++i;"", ""} catch (Exception) {"", ""var b = false; b = !b;//will never get here"", ""}""]
+        }
+    ]
+}";
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        [Test]
+        public void OffsetExcludes()
+        {
+            string offsets;
+            if (onDotNet)
+                offsets = "14, 15, 16, 17";
+            else
+                offsets = "9, 10, 11, 12, 13";
+
+            var config =
+string.Format(@"{{
+    ""assemblies"": [""bin/Debug/TestTarget.exe""],
+    ""typeInclude"": "".*TestTarget"",
+    ""methodInclude"": "".*OffsetExcludes.*"",
+    ""methodBodyExcludes"": [
+        {{
+            ""method"": ""System.Void Gaillard.SharpCover.Tests.TestTarget::OffsetExcludes()"",
+            ""offsets"": [{0}]
+        }}
+    ]
+}}", offsets);
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        //to get an IL instruction that uses a prefix instruction like constrained
+        [Test]
+        public void Constrained()
+        {
+            var config = @"{""assemblies"": [""bin/Debug/TestTarget.exe""], ""typeInclude"": "".*Constrained""}";
+
+            Assert.AreEqual(0, Program.Main(new []{ "instrument", config }));
+
+            Process.Start(testTargetExePath).WaitForExit();
+
+            Assert.AreEqual(0, Program.Main(new []{ "check" }));
+
+            Assert.IsTrue(File.ReadLines(Program.RESULTS_FILENAME).Any());
+        }
+
+        [Test]
+        public void MissingCommand()
+        {
+            Assert.AreEqual(2, Program.Main(new string[0]));
+        }
+
+        [Test]
+        public void BadCommand()
+        {
+            Assert.AreEqual(2, Program.Main(new []{ "BAD_COMMAND" }));
+        }
+
+        [Test]
+        public void MissingConfig()
+        {
+            Assert.AreEqual(2, Program.Main(new []{ "instrument" }));
+        }
+    }
+}
---
+++ TestFramework/Tools/Gaillard.SharpCover.Tests/bin/Debug/TestTarget.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace Gaillard.SharpCover.Tests
+{
+    public interface IEvent
+    {
+        event EventHandler TheEvent;
+    }
+
+    public class EventUsage : IEvent
+    {
+        public event EventHandler TheEvent;
+
+        public void EventMethod(object sender, EventArgs e)
+        {
+            var i = 0;
+            i += 1;
+        }
+
+        public void RaiseEvent()
+        {
+            TheEvent(null, null);
+        }
+    }
+
+    public sealed class TestTarget
+    {
+        public sealed class Nested
+        {
+            public void Covered()
+            {
+                var i = 0;
+                ++i;
+            }
+        }
+
+        public static void UncoveredIf()
+        {
+            var i = 0;
+            if (i == 0)
+                ++i;
+            else
+                --i;
+        }
+
+        public static void UncoveredLeave()
+        {
+            var i = 0;
+            ++i;
+            try {
+                if (i == 1)
+                    throw new Exception();
+                //miss a leave from this try here.
+            } catch (Exception) {
+                var j = 1;
+                --j;
+            }
+        }
+
+        public static void OffsetExcludes()
+        {
+            var i = 0;
+            if (i == 1)
+                ++i;
+        }
+
+        public static void LineExcludes()
+        {
+            var i = 0;
+            if (i == 1)
+                ++i;
+
+            try {
+                --i;
+            } catch (Exception) {
+                var b = false; b = !b;//will never get here
+            }
+        }
+
+        //different bits c# syntax to exercise different instructions and jumps etc
+        public void Covered()
+        {
+            var i = 0;
+            ++i;
+
+            try {
+                --i;
+            } finally {
+                i += 2;
+            }
+
+            int j;
+            goto There;
+            There:
+            j = 1234;
+            ++j;
+
+            foreach (var k in new [] { "boo", "foo" }) {
+                k.EndsWith("oo");
+            }
+
+            for (var k = 0; k < 2; ++k)
+                k += 2;
+
+            {
+                var k = 2;
+                while (k != 0)
+                    --k;
+            }
+
+            for (var k = 0; k < 2; ++k) {
+                var b = k % 2 == 0 ? true : false;
+                b &= b;
+            }
+
+            for (var k = 0; k < 4; ++k) {
+                switch (k) {
+                case 0:
+                    break;
+                case 1:
+                case 2:
+                    break;
+                }
+            }
+
+            Func<bool> func = () => true;
+
+            func();
+
+            {
+                var b = (object)false;
+                b = (bool)b;
+            }
+
+            using (var disposable = new Disposable()) {
+                var b = true;
+                b = !b;
+            }
+        }
+
+        private sealed class Disposable : IDisposable
+        {
+            public void Dispose() { }
+        }
+
+        private sealed class Constrained
+        {
+            public string ToString<T>(T value) where T : struct
+            {
+                return value.ToString();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            new TestTarget().Covered();
+            new Nested().Covered();
+
+            UncoveredIf();
+            UncoveredLeave();
+            OffsetExcludes();
+            LineExcludes();
+
+            var eventUsage = new Gaillard.SharpCover.Tests.EventUsage();
+            eventUsage.TheEvent += eventUsage.EventMethod;
+            eventUsage.RaiseEvent();
+            eventUsage.TheEvent -= eventUsage.EventMethod;
+
+            new Constrained().ToString(5);
+        }
+    }
+}
