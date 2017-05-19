f37256c915a5152a02fb037fa4cbbbe83d532bc7
tejasranade <tejas.ranade@gmail.com>
2017-05-19 14:28:25 -0400

    test script changes

--- TestFramework/Tools/build.sh
+++ TestFramework/Tools/build.sh
@@ -1,4 +1,4 @@
-xbuild /p:Configuration=Release Kinvey-Xamarin/Kinvey-Xamarin.csproj \
+xbuild /p:Configuration=Release Kinvey.Core/Kinvey.Core.csproj \
 && xbuild /p:Configuration=Release TestFramework/Tests.Integration/Tests.Integration.csproj \
 && mono TestFramework/Tools/Gaillard.SharpCover/bin/Debug/SharpCover.exe instrument TestFramework/Tools/config.json \
 && mono packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe TestFramework/Tests.Integration/bin/Release/Tests.Integration.dll \
