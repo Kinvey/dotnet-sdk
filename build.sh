7066ae8e02ed4278063a9760cb51759f5750cecf
Tejas Ranade <tejas.ranade@gmail.com>
2017-05-19 13:50:16 -0400

    Updated build.sh for naming changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-xbuild Kinvey-Xamarin/Kinvey-Xamarin.csproj \
+xbuild Kinvey.Core/Kinvey.Core.csproj \
 && xbuild TestFramework/Tests.Integration/Tests.Integration.csproj \
 && mono TestFramework/Tools/Gaillard.SharpCover/bin/Debug/SharpCover.exe instrument config.json \
 && mono packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe TestFramework/Tests.Integration/bin/Debug/Tests.Integration.dll \
