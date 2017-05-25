b619d980df03e3237bbdade94e1d1731b7654edc
tejasranade <tejas.ranade@gmail.com>
2017-05-25 17:17:10 -0400

    New pull request with automatic pagination

--- Kinvey.Core/Store/DataStore.cs
+++ Kinvey.Core/Store/DataStore.cs
@@ -51,6 +51,7 @@ namespace Kinvey
 		/// <value><c>true</c> if delta set fetching enabled; otherwise, <c>false</c>.</value>
 		public bool DeltaSetFetchingEnabled { get; set; }
 
+		public bool AutoPagination { get; set; }
 		/// <summary>
 		/// Represents the name of the collection.
 		/// </summary>
@@ -136,6 +137,7 @@ namespace Kinvey
 			this.customRequestProperties = this.client.GetCustomRequestProperties();
 			this.networkFactory = new NetworkFactory(this.client);
 			this.DeltaSetFetchingEnabled = false;
+			this.AutoPagination = false;
 		}
 
 		#region Public interface
@@ -361,8 +363,15 @@ namespace Kinvey
 				throw new KinveyException(EnumErrorCategory.ERROR_DATASTORE_NETWORK, EnumErrorCode.ERROR_DATASTORE_PULL_ONLY_ON_CLEAN_SYNC_QUEUE, "");
 			}
 
-			//TODO query
-			PullRequest<T> pullRequest = new PullRequest<T> (client, CollectionName, cache, DeltaSetFetchingEnabled, query);
+			if (AutoPagination)
+			{
+				var pagedPullRequest = new PagedPullRequest<T>(client, CollectionName, cache, DeltaSetFetchingEnabled, query);
+				ct.ThrowIfCancellationRequested();
+				return await pagedPullRequest.ExecuteAsync();
+
+			}
+
+			var	pullRequest = new PullRequest<T> (client, CollectionName, cache, DeltaSetFetchingEnabled, query);	
 			ct.ThrowIfCancellationRequested();
 			return await pullRequest.ExecuteAsync();
 		}
---
+++ Kinvey.Core/Store/PagedPullRequest.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) 2016, Kinvey, Inc. All rights reserved.
+//
+// This software is licensed to you under the Kinvey terms of service located at
+// http://www.kinvey.com/terms-of-use. By downloading, accessing and/or using this
+// software, you hereby accept such terms of service  (and any agreement referenced
+// therein) and agree that you have read, understand and agree to be bound by such
+// terms of service and are of legal age to agree to such terms with Kinvey.
+//
+// This software contains valuable confidential and proprietary information of
+// KINVEY, INC and is subject to applicable licensing agreements.
+// Unauthorized reproduction, transmission or distribution of this file and its
+// contents is a violation of applicable laws.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Kinvey
+{
+	public class PagedPullRequest<T> : ReadRequest<T, PullDataStoreResponse<T>>
+	{
+		BlockingCollection<List<T>> workQueue = new BlockingCollection<List<T>>(10);
+
+		public PagedPullRequest(AbstractClient client, string collection, ICache<T> cache, bool deltaSetFetchingEnabled, IQueryable<object> query)
+			: base(client, collection, cache, query, ReadPolicy.FORCE_NETWORK, deltaSetFetchingEnabled)
+		{
+
+		}
+
+		public override async Task<PullDataStoreResponse<T>> ExecuteAsync()
+		{
+			int skipCount = 0, pageSize = 10000;
+
+			var count = await new GetCountRequest<T>(this.Client, this.Collection, this.Cache, ReadPolicy.FORCE_NETWORK, false, null, this.Query).ExecuteAsync();
+
+			Task consumer = Task.Run(() => FlushWorkQueue());
+
+			var pageQueue = new List<Task<List<T>>>();
+			do
+			{
+				var skipTakeQuery = this.Query.Skip(skipCount).Take(pageSize);
+				pageQueue.Add(new FindRequest<T>(Client, Collection, Cache, ReadPolicy.FORCE_NETWORK, false, null, skipTakeQuery, null).ExecuteAsync());
+				skipCount += pageSize;
+			} while (skipCount < count);
+
+
+			while (pageQueue.Count > 0) {
+				Debug.WriteLine("Pagequeue size: " + pageQueue.Count);
+				var page = await Task.WhenAny(pageQueue);
+				pageQueue.Remove(page);
+				try
+				{
+					workQueue.Add(await page);
+				}
+				catch (OperationCanceledException e)
+				{
+					//log 
+				}
+				catch (Exception e) 
+				{ 
+					//return all exceptions once done
+				}
+			}
+			workQueue.CompleteAdding();
+			await consumer;
+			return new PullDataStoreResponse<T>();
+		}
+
+		private void FlushWorkQueue()
+		{
+
+			while (true)
+			{
+				try
+				{
+					List<T> items = workQueue.Take();
+					Cache.RefreshCache(items);
+					Debug.WriteLine(string.Format("Processing {0} items, workQueue size = {1}", items.Count, workQueue.Count));
+				}
+				catch (InvalidOperationException e)
+				{
+					Debug.WriteLine(string.Format("Work queue has been closed."));
+					break;
+				}
+			}
+		}
+
+		public override Task<bool> Cancel()
+		{
+			throw new KinveyException(EnumErrorCategory.ERROR_GENERAL, EnumErrorCode.ERROR_METHOD_NOT_IMPLEMENTED, "Cancel method on PullRequest not implemented.");
+		}
+	}
+}
