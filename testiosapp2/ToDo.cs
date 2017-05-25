d7661c0ead5c41407b79ebc4a854ea5e0e5b7d49
Vinay Gahlawat <vinay@kinvey.com>
2017-05-25 16:48:00 -0400

    MLIBZ-1839 Add ToDo model object to test app

---
+++ testiosapp2/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Kinvey;
+
+namespace testiosapp2
+{
+	[JsonObject(MemberSerialization.OptIn)]
+	public class ToDo : Entity
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("details")]
+		public string Details { get; set; }
+
+		[JsonProperty("due_date")]
+		public string DueDate { get; set; }
+
+		[JsonProperty("value")]
+		public int Value { get; set; }
+
+		[JsonProperty("bool_value")]
+		public bool BoolVal { get; set; }
+	}
+}
+
