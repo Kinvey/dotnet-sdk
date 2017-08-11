cd1c112364e128268fd2cbc4f4bdf815659371f5
Vinay Gahlawat <vinay@kinvey.com>
2017-08-11 17:38:31 -0400

    Xamarin 3.0.14 Release

--- Kinvey.Core/Core/KinveyHeaders.cs
+++ Kinvey.Core/Core/KinveyHeaders.cs
@@ -25,7 +25,7 @@ namespace Kinvey
 		/// <summary>
 		/// The version of the SDK.
 		/// </summary>
-		public static string VERSION = "3.0.13";
+		public static string VERSION = "3.0.14";
 
 		// The kinvey API version.
         static string kinveyApiVersionKey = "X-Kinvey-API-Version";
