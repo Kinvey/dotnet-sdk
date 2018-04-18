542a10674fdc8fa4e685d2802be4c2aa100b6804
Vinay Gahlawat <vinay@kinvey.com>
2018-04-18 12:04:27 -0400

    MLIBZ-2417 Implementation of device info header for dotnet.

---
+++ Kinvey.Net/Client/DotnetNativeClientBuilder.cs
@@ -0,0 +1,13 @@
+﻿using SQLite.Net.Platform.Win32;
+
+namespace Kinvey
+{
+    public class DotnetNativeClientBuilder : Client.Builder
+    {
+        public DotnetNativeClientBuilder(string appKey, string appSecret) :
+        base(appKey, appSecret, Constants.DevicePlatform.NET)
+        {
+            this.setOfflinePlatform(new SQLitePlatformWin32());
+        }
+    }
+}
