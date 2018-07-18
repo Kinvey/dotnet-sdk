32322431624c648987937dcc165660e20018f5f8
Vinay Gahlawat <vinay@kinvey.com>
2018-07-18 05:41:12 -0400

    .NET Release 3.1.3

--- Kinvey.Core/Core/KinveyHeaders.cs
+++ Kinvey.Core/Core/KinveyHeaders.cs
@@ -26,7 +26,7 @@ namespace Kinvey
 		/// <summary>
 		/// The version of the SDK.
 		/// </summary>
-		public static string VERSION = "3.1.2";
+		public static string VERSION = "3.1.3";
 
 		// The kinvey API version.
         static string kinveyApiVersionKey = "X-Kinvey-API-Version";
