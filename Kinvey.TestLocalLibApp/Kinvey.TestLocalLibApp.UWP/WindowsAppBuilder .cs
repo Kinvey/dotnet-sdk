413ece7b78535c2e3bcf920751530bf03a3c7202
Roman <roman.ogolikhin@softteco.com>
2018-10-26 09:50:47 +0300

    Set path for UWP explicitly.

--- Kinvey.TestLocalLibApp/Kinvey.TestLocalLibApp.UWP/WindowsAppBuilder .cs
+++ Kinvey.TestLocalLibApp/Kinvey.TestLocalLibApp.UWP/WindowsAppBuilder .cs
@@ -11,7 +11,7 @@ namespace Kinvey.TestLocalLibApp.UWP
     {
         public Client.Builder GetBuilder()
         {
-            return new Client.Builder(Constants.Settings.AppKey, Constants.Settings.AppSecret);
+            return new Client.Builder(Constants.Settings.AppKey, Constants.Settings.AppSecret).SetFilePath(Windows.Storage.ApplicationData.Current.LocalFolder.Path);
         }
     }
 }
