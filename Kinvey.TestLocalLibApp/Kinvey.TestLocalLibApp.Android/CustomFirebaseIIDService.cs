e03187520dd2937a361aa24e8aeec25fe11eb8de
Roman <roman.ogolikhin@softteco.com>
2018-11-29 14:41:58 +0300

    Adding FCM.

---
+++ Kinvey.TestLocalLibApp/Kinvey.TestLocalLibApp.Android/CustomFirebaseIIDService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+using Firebase.Messaging;
+
+namespace Kinvey.TestLocalLibApp.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class CustomFirebaseIIDService : FirebaseInstanceIdService
+    {
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            FirebaseMessaging.Instance.SubscribeToTopic("news");
+        }
+    }
+}
--- Kinvey.TestLocalLibApp/Kinvey.TestLocalLibApp.Android/MainActivity.cs
+++ Kinvey.TestLocalLibApp/Kinvey.TestLocalLibApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase.Messaging;
 
 namespace Kinvey.TestLocalLibApp.Droid
 {
@@ -19,7 +20,7 @@ namespace Kinvey.TestLocalLibApp.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App());          
         }
     }
 }
