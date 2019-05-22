36eabdcc7279e0da228bdbb65881dab6a13c1a6c
Eugen <e.goroluov@softteco.com>
2019-05-22 17:16:47 +0300

    The empty info for exception.

--- Kinvey.Core/Store/DataStore.cs
+++ Kinvey.Core/Store/DataStore.cs
@@ -355,7 +355,7 @@ namespace Kinvey
         {
             if (!int.TryParse(KinveyClient.ApiVersion, out int apiVersion) || apiVersion < 5)
             {
-                throw new KinveyException(EnumErrorCategory.ERROR_GENERAL, EnumErrorCode.ERROR_DATASTORE_NOT_COMPATIBLE_KINVEY_API_VERSION, "Query cannot be null.");
+                throw new KinveyException(EnumErrorCategory.ERROR_GENERAL, EnumErrorCode.ERROR_DATASTORE_NOT_COMPATIBLE_KINVEY_API_VERSION, string.Empty);
             }
 
             var request = new MultiInsertRequest<T, KinveyDataStoreResponse<T>>(entities, this.client, this.CollectionName, this.cache, this.syncQueue, this.storeType.WritePolicy);
