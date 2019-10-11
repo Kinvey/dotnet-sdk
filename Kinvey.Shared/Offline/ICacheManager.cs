58f7aaa30fe417ffb93a1ebd7075c07db6c7d6d7
Eugen <e.goroluov@softteco.com>
2019-10-11 12:20:36 +0300

    improvement: addition of missing XML comments for documentation.

--- Kinvey.Shared/Offline/ICacheManager.cs
+++ Kinvey.Shared/Offline/ICacheManager.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Kinvey, Inc. All rights reserved.
+// Copyright (c) 2019, Kinvey, Inc. All rights reserved.
 //
 // This software is licensed to you under the Kinvey terms of service located at
 // http://www.kinvey.com/terms-of-use. By downloading, accessing and/or using this
@@ -12,37 +12,62 @@
 // contents is a violation of applicable laws.
 
 using System;
-using SQLite;
 
 namespace Kinvey
 {
+    /// <summary>
+    /// Interface for cache manager.
+    /// </summary>
     public interface ICacheManager : IDisposable
 	{
-		/// <summary>
-		/// Gets or sets the dbpath.
-		/// </summary>
-		/// <value>The dbpath.</value>
-		string dbpath{ get; set;}
+        /// <summary>
+        /// Gets or sets the database file path.
+        /// </summary>
+        /// <value>The database file path.</value>
+        string dbpath { get; set;}
 
-		/// <summary>
-		/// Gets the cache.
-		/// </summary>
-		/// <returns>The cache.</returns>
-		/// <param name="collectionName">Collection name.</param>
-		ICache<T> GetCache <T>(string collectionName) where T : class, new();
+        /// <summary>
+        /// Gets the cache.
+        /// </summary>
+        /// <returns>The cache.</returns>
+        /// <param name="collectionName">Collection name.</param>
+        /// <typeparam name="T">The type of an item.</typeparam>
+        ICache<T> GetCache <T>(string collectionName) where T : class, new();
 
-		ISyncQueue GetSyncQueue (string collectionName);
+        /// <summary>
+        /// Gets the synchronization queue.
+        /// </summary>
+        /// <returns>The synchronization queue.</returns>
+        /// <param name="collectionName">Collection name.</param>
+        /// <typeparam name="T">The type of an item.</typeparam>
+        ISyncQueue GetSyncQueue (string collectionName);
 
+        /// <summary>
+        /// Gets query cache item.
+        /// </summary>
+        /// <returns>Query cache item.</returns>
+        /// <param name="collectionName">Collection name.</param>
+        /// <param name="query">Query.</param>
+        /// <param name="lastRequestTime">The last request time.</param>
         QueryCacheItem GetQueryCacheItem(string collectionName, string query, string lastRequestTime);
 
+        /// <summary>
+        /// Sets query cache item.
+        /// </summary>
+        /// <returns><c>True</c> if the query cache item was inserted; otherwise, <c>false</c>.</returns>
+        /// <param name="item">Query cache item.</param>
         bool SetQueryCacheItem(QueryCacheItem item);
 
+        /// <summary>
+        /// Deletes query cache item.
+        /// </summary>
+        /// <returns><c>True</c> if the query cache item was deleted; otherwise, <c>false</c>.</returns>
+        /// <param name="item">Query cache item.</param>
         bool DeleteQueryCacheItem(QueryCacheItem item);
 
 		/// <summary>
 		/// Clears the storage.
 		/// </summary>
 		void clearStorage();
-
 	}
 }
